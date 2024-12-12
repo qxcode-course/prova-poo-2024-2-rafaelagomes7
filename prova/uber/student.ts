d158391de5e821c95d9b7c55592f43cf3903228a
Rafaela Gomes Resende <rafaelagomesr@alu.ufc.br>
2024-12-12 01:06:26 +0000

    uber

--- prova/uber/student.ts
+++ prova/uber/student.ts
@@ -3,20 +3,108 @@ function input(): string { let X: any = input; X.L = X.L || require("fs").readFi
 function write(text: any, endl="\n") { process.stdout.write("" + text + endl); }
 export {};
 
-class Adapter {
-    setDriver(name: string, money: number): void {
+class Pessoa {
+    private name: string;
+    private money: number;
+
+    constructor(name: string, money: number){
+        this.name = name;
+        this.money = money;
+    }
+    public getName(): string{
+        return this.name;
+    }
+    public getMoney(): number{
+        return this.money;
+    }
+    public setName(value: string): void{
+        this.name = value;
+    }
+    public setMoney(value: number): void{
+        if(this.money <= 0){
+            this.money = 0;
+        }
+        this.money += value; 
+    }
+    toString(): string{
+        return `${this.name}:${this.money}`
+    }
+}
+class Moto{
+    private cost: number;
+    private driver: Pessoa| null;
+    private passenger: Pessoa | null; 
+
+    constructor(){
+        this.cost = 0;
+        this.driver = null;
+        this.passenger = null;
+    }
+    public getPassenger(): Pessoa| null{
+        return this.passenger;
+    }
+    public getDriver(): Pessoa | null{
+        return this.driver;
+    }
+    public getCost(): number{
+        return this.cost;
+    }
+    public setPassenger(name: Pessoa | null): void{
+       this.passenger = name;
+    }
+    public setDriver(name: Pessoa): void {
+        this.driver = name;
+    }
+    public setCost(value: number): void{
+        this.cost += value;
+    }
+    public drive(distance: number): void{
+        this.setCost(distance);
+    }
+    public leavePass(): void{
+        this.passenger?.setMoney(-this.getCost());
+        if(this.passenger?.getMoney() != null){
+            if(this.passenger.getMoney() <= 0){
+                console.log("fail: Passenger does not have enough money")
+                this.passenger?.setMoney(0);
+            }
+        }
+        this.driver?.setMoney(this.getCost());
+        console.log(this.getPassenger() + " leave");
+        this.setCost(-this.getCost());
+        this.setPassenger(null);
+    }
+    toString(): string {
+        if(this.getDriver() == null) {
+            return `Cost: ${this.getCost()}, Driver: None, Passenger: None`;
+        }
+        if(this.getPassenger() == null) {
+            return `Cost: ${this.getCost()}, Driver: ${this.getDriver()}, Passenger: None`;
+        }
+        return `Cost: ${this.getCost()}, Driver: ${this.getDriver()}, Passenger: ${this.getPassenger()}`;
     }
 
+}
+
+class Adapter {
+    moto: Moto = new Moto();
+
+    setDriver(name: string, money: number): void {
+        this.moto.setDriver(new Pessoa(name, money));
+    }
     setPassenger(name: string, money: number): void {
+        this.moto.setPassenger(new Pessoa(name, money))
     }
 
     drive(distance: number): void {
+        this.moto.drive(distance)
     }
-
     leavePassenger(): void {
+        this.moto.leavePass();
     }
 
     show(): void {
+        console.log(this.moto.toString())
     }
 }
 
@@ -24,8 +112,7 @@ function main(): void {
     let adapter: Adapter = new Adapter();
     while (true) {
         write("$", "");
-        const line = input();
-        const args = line.split(" ");
+        const line = input();        const args = line.split(" ");
         write(line);
 
         if      (args[0] === "end"      ) { break;                                   }
